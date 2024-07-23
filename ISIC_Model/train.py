d563cba3f74459c981a4780af1f9ef7199c2f242
Abiel Almonte <almonteabiel@gmail.com>
2024-07-22 20:18:27 -0400

    refactor: move function back to util feat: make testing option

--- ISIC_Model/train.py
+++ ISIC_Model/train.py
@@ -1,35 +1,29 @@
+from util import ResUNetWithTabular, ResUNet, load_config,\
+    set_seed, prepare, train_evaluate_test
 from sklearn.model_selection import StratifiedGroupKFold
-from util import ResUNetWithTabular, ResUNet,\
-                train_evaluate_model, oversample
-import warnings, pandas, yaml, os, torch, numpy
+import warnings, pandas, os, torch, torch.cuda
 
 warnings.filterwarnings('ignore')
-
-def load_config(fp):
-    with open(fp, 'r') as file:
-        return yaml.safe_load(file)
-    
-def set_seed(seed: int= 42):
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    numpy.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
 config= load_config(os.path.join(os.path.dirname(__file__), 'config.yaml'))
-metadata= pandas.read_csv(os.path.join(os.path.dirname(__file__), config['data']['metadata_file']))
-metadata= oversample(metadata)
-
 set_seed(config['seed'])
 
+model_name= config['model']['name']
+metadata= pandas.read_csv(os.path.join(os.path.dirname(__file__), config['data']['metadata_file']))
+metadata, metadata_test= prepare(metadata,  config['seed'])
 splits= list(StratifiedGroupKFold(n_splits=2).split(metadata, metadata['target'], groups= metadata['patient_id']))
 
-if config['model']['name'] == 'ResUNetWithTabular':
+if model_name == 'ResUNetWithTabular':
     model = ResUNetWithTabular(config['model']['cont_features'], config['model']['bin_features'])
-elif config['model']['name'] == 'ResUNet':
+elif model_name == 'ResUNet':
     model = ResUNet()
 else:
-    raise ValueError(f"Unsupported model: {config['model']['name']}")
+    raise ValueError(f"Unsupported model: {model_name}")
+    
+train_evaluate_args= {'data': metadata, 'config': config, 'splits': splits, 'model': model}
+if config['testing']['test']:
+    test_args= {'data': metadata_test, 'config': config, 'model': model}
+else: test_args= None
 
-train_evaluate_model(metadata, config, splits, model)
+pAUC= train_evaluate_test(train_evaluate_args, test_args)
+if pAUC:
+    print(f"pAUC: {pAUC:.3f}")
