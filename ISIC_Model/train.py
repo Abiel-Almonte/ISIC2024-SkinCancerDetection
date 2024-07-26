5eb8d5409c3776b7d0115cbe7520f276c84db024
Abiel Almonte <almonteabiel@gmail.com>
2024-07-26 09:11:51 -0400

    chore: rename classes

--- ISIC_Model/train.py
+++ ISIC_Model/train.py
@@ -1,5 +1,5 @@
-from util import ResUNetWithTabular, ResUNet, load_config,\
-    set_seed, prepare, train_evaluate_test
+from util import EfficientUNetWithTabular, EfficientUNet, load_config,\
+    set_seed, prepare, train_evaluate_test, test_model, oversample
 from sklearn.model_selection import StratifiedGroupKFold
 import warnings, pandas, os
 
@@ -13,9 +13,9 @@ metadata, metadata_test= prepare(metadata, config['testing']['test_size'], confi
 splits= list(StratifiedGroupKFold(n_splits=2).split(metadata, metadata['target'], groups= metadata['patient_id']))
 
 if model_name == 'ResUNetWithTabular':
-    model = ResUNetWithTabular(config['model']['cont_features'], config['model']['bin_features'])
+    model = EfficientUNetWithTabular(config['model']['cont_features'], config['model']['bin_features'])
 elif model_name == 'ResUNet':
-    model = ResUNet()
+    model = EfficientUNet()
 else:
     raise ValueError(f"Unsupported model: {model_name}")
     
@@ -23,7 +23,7 @@ train_evaluate_args= {'data': metadata, 'config': config, 'splits': splits, 'mod
 if config['testing']['test']:
     test_args= {'data': metadata_test, 'config': config, 'model': model}
 else: test_args= None
-
+#pAUC= test_model(metadata_test, config, model)
 pAUC= train_evaluate_test(train_evaluate_args, test_args)
 if pAUC:
     print(f"pAUC: {pAUC:.3f}")
