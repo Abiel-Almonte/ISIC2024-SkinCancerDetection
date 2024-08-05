1ae8072225bf4ef057f363fb7aeaaa68fa185b60
Abiel Almonte <almonteabiel@gmail.com>
2024-08-05 00:12:46 -0400

    style: change abstract model name

--- model/architectures/registry.py
+++ model/architectures/registry.py
@@ -1,14 +1,14 @@
-from base import ModelProtocol
+from .base import ISICModel
 from typing import Type, Dict, Tuple, Any
 
 __all__= ['model_registry', 'register_model', 'get_model']
 
-model_registry:  Dict[str, Tuple[Type[ModelProtocol], Dict[str, Any]]] = {}
+model_registry:  Dict[str, Tuple[Type[ISICModel], Dict[str, Any]]] = {}
 
-def register_model(name: str, ModelClass: Type[ModelProtocol], args: Dict[str, Any]):
-    model_registry[name] = {ModelClass, args}
+def register_model(name: str, ModelClass: Type[ISICModel], args: Dict[str, Any]):
+    model_registry[name] = (ModelClass, args)
 
-def get_model(name: str) -> Type[ModelProtocol]:
+def get_model(name: str) -> Type[ISICModel]:
     if name not in model_registry:
         raise ValueError(f"Model {name} not found. Choose one of the following: {list(model_registry.keys())}")
     return model_registry[name]
