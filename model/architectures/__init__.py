0fec09ef7f71cf8478a45ceae0239e53e9829f40
Abiel Almonte <almonteabiel@gmail.com>
2024-08-04 21:18:05 -0400

    chore: registering isic models and arch imports

---
+++ model/architectures/__init__.py
@@ -0,0 +1,20 @@
+from .unet_tabnet import *
+from .transformer import *
+from .vae_tabnet import *
+from .base import *
+
+__all__ = []
+__all__.extend(unet_tabnet.__all__)
+__all__.extend(transformer.__all__)
+__all__.extend(vae_tabnet.__all__)
+__all__.extend(base.__all__)
+
+from .registry import *
+
+register_model('EfficientUNetWithTabular', EfficientUNetWithTabular, {'cont_features': 31, 'bin_features': 6})
+register_model('EfficientUNetWithTabular_v2', EfficientUNetWithTabular_v2, {'cont_features': 31, 'bin_features': 6})
+register_model('ViTWithTabular', ViTWithTabular, {'cont_features': 31, 'bin_features': 6})
+register_model('VAETabNet', VAETabNet, {'cont_features': 31, 'bin_features': 6, 'latent_dim': 256})
+register_model('CrossModalTransformer', CrossModalTransformer, {'cont_features': 31, 'bin_features': 6, 'dim':6, 'depth':768, 'mlp_dim': 3072})
+
+__all__.extend(registry.__all__)
