669d1b9cfc92bb51e8a32d0b4b80da3de32dfd83
Abiel Almonte <almonteabiel@gmail.com>
2024-08-17 23:43:11 -0400

    chore: change tabular size

--- model/test.py
+++ model/test.py
@@ -4,10 +4,11 @@ from architectures import EfficientNetEVAModel
 
 if '__main__' == __name__:
     image= torch.randn(16, 3, 336, 336)
-    cont_tabular= torch.randn(16, 31)
-    bin_tabular=  torch.randint(0, 5, (16, 1))
+    cont_tabular= torch.randn(16, 36)
+    bin_tabular=  torch.randint(0, 6, (16, 1))
 
 
     model= EfficientNetEVAModel()
     output= model(image, cont_tabular, bin_tabular)
+    
     summary(model, input_size= [(3, 336, 336), (31,), (5,)],  batch_dim=32, depth=3, verbose=1)
