a3e7e8dc4f488542a43f25a642e6b652a501a9b8
Abiel Almonte <almonteabiel@gmail.com>
2024-08-20 22:22:43 -0400

    refactor: rename model and file to correct name

--- model/architectures/lightnn.py
+++ model/architectures/lightnn.py
@@ -5,10 +5,18 @@ import torch.nn as nn
 from .models import TabNet
 from .base import ISICModel
 
-__all__= ['EfficientNetEVAModel']
+__all__= ['LightMultiModalNN']
 
-class EfficientNetEVAModel(nn.Module, ISICModel):
-    def __init__(self):
+class LightMultiModalNN(nn.Module, ISICModel):
+    def __init__(
+        self, 
+        tabular_hidden_dim:int= 128,
+        tabular_out_dim:int= 384,
+        tabular_dropout:float= 0.3,
+        classifier_hidden_dim:int= 64,
+        classifier_dropout:float= 0.2,
+    )-> None:
+        
         super().__init__()
 
         self.model_eva = timm.create_model( 
@@ -19,24 +27,23 @@ class EfficientNetEVAModel(nn.Module, ISICModel):
         self.model_eva.head = nn.Identity()
 
         self.tabular_net = nn.Sequential(
-            nn.Linear(31+ 6, 128),
+            nn.Linear(31+ 6, tabular_hidden_dim),
             nn.SiLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, 380),
+            nn.Dropout(tabular_dropout),
+            nn.Linear(tabular_hidden_dim, tabular_out_dim),
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features + 380, 64),
+            nn.Linear(in_features + tabular_out_dim, classifier_hidden_dim),
             nn.GELU(),  
-            nn.Dropout(0.2),
-            nn.Linear(64, 1),
+            nn.Dropout(classifier_dropout),
+            nn.Linear(classifier_hidden_dim, 1),
         )
 
     def forward(self, image: torch.Tensor, continuous:torch.Tensor,  binary:torch.Tensor)-> torch.Tensor:
         eva_out= self.model_eva(image)
         tab_out = self.tabular_net(torch.cat([continuous, binary], dim=1))
         combined = torch.cat([eva_out, tab_out], dim=1)
-
 
         out = self.classifier(combined)
         return out
