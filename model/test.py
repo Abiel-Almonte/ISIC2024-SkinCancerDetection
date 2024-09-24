8c55574a160b92ceca40a1fb8ffddef78dac28ce
Abiel Almonte <almonteabiel@gmail.com>
2024-09-23 20:10:34 -0400

    refactor: change lightmultimodalnn name

--- model/test.py
+++ model/test.py
@@ -1,14 +1,14 @@
 import torch 
 from torchsummary import summary
-from architectures import LightMutiModalNN
+from architectures import LightMultiModalNN
 
 if '__main__' == __name__:
-    image= torch.randn(16, 3, 336, 336)
-    cont_tabular= torch.randn(16, 36)
-    bin_tabular=  torch.randint(0, 6, (16, 1))
+    image= torch.randn(16, 3, 256, 256, device='cuda')
+    cont_tabular= torch.randn(16, 36, device='cuda')
+    bin_tabular=  torch.randint(0, 6, (16, 1), device='cuda')
 
 
-    model= LightMutiModalNN()
+    model= LightMultiModalNN()
+    model.to('cuda')
     output= model(image, cont_tabular, bin_tabular)
-    
-    summary(model, input_size= [(3, 336, 336), (31,), (5,)])
+    #summary(model, input_size= [(3, 336, 336), (31,), (6,)])
